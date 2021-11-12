4013787c2f9d3677aad1509ff26937db25825538
yj7o5 <yawarjamal@gmail.com>
2021-11-12 10:38:42 +0500

    update heading line on main page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site under construction - migrating blog from netlify to cloudflare
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="https://yjamal.netlify.app/">Old Site</a>
       </header>
     </div>
   );
